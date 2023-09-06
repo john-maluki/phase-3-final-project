3c9b0c303311db575c0fde01e4ea777b828bddfa
john <johnmaluki58@gmail.com>
2023-09-06 16:13:13 +0300

    Update env base metadata to a single import

--- app/db/migrations/env.py
+++ app/db/migrations/env.py
@@ -18,11 +18,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models.facilities import Base as facility
-from models.adopters import Base as adopter
-from models.dogs import Base as dog
+from models import Base
 
-target_metadata = [facility.metadata, adopter.metadata, dog.metadata]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -68,9 +66,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
